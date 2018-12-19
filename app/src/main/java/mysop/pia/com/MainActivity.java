485139f0571183b04bcab135487e7829e1a97a58
Eric Good <ericdgood@yahoo.com>
2018-12-18 19:46:54 -0500

    feat: Added category adapter
    - Added category adapter and inflated 5 test categories

--- app/src/main/java/mysop/pia/com/MainActivity.java
+++ app/src/main/java/mysop/pia/com/MainActivity.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.gridview_categories)
+    RecyclerView gridviewCategories;
+    CategoryGridAdapter mCategoryGridAdpater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        gridviewCategories.setLayoutManager( new GridLayoutManager(this, 2));
+        mCategoryGridAdpater = new CategoryGridAdapter(this);
+        gridviewCategories.setAdapter(mCategoryGridAdpater);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,16 +47,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
