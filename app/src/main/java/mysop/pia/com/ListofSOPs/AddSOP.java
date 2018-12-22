980af79d78cdbd8f62e56e6bf4ea1b4da0d063ba
Eric Good <ericdgood@yahoo.com>
2018-12-21 20:56:02 -0500

    feat: layout for new sop
    -created layout for sop from intent from list of sops

--- app/src/main/java/mysop/pia/com/ListofSOPs/AddSOP.java
+++ app/src/main/java/mysop/pia/com/ListofSOPs/AddSOP.java
@@ -1,6 +1,18 @@
 package mysop.pia.com.ListofSOPs;
 
-public class AddSOP {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import mysop.pia.com.R;
 
+public class AddSOP extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_sop);
+        ButterKnife.bind(this);
+    }
 }
