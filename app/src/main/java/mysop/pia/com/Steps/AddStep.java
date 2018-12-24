27a450a8c7f9054461d9ba0a4961a94d59ea0af0
Eric Good <ericdgood@yahoo.com>
2018-12-24 12:16:50 -0500

    Feat: get image from gallery or camera
    - Added intents and requests for gallery access and camera access.
    
    - Image is selected or take and saved to database

--- app/src/main/java/mysop/pia/com/Steps/AddStep.java
+++ app/src/main/java/mysop/pia/com/Steps/AddStep.java
@@ -1,10 +1,18 @@
 package mysop.pia.com.Steps;
 
+import android.Manifest;
 import android.arch.persistence.room.Room;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +20,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,8 +54,18 @@ public class AddStep extends AppCompatActivity {
     @BindView(R.id.imageview_add_step_image_preview)
     ImageView imageviewImagePreview;
 
-    public static final int PICK_IMAGE = 1;
     Uri imageUri;
+    int PERMISSION_GALLERY = 1;
+    int PERMISSION_CAMERA = 2;
+    String mCameraFileName;
+    String[] GALLERY_PERMISSIONS = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    String[] CAMERA_PERMISSIONS = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,12 +99,12 @@ public class AddStep extends AppCompatActivity {
         });
     }
 
-    private void AddStepToRoomDatabase(String sopTitle, int stepNumber){
+    private void AddStepToRoomDatabase(String sopTitle, int stepNumber) {
         String stepTitle = ediitTextStepTitle.getText().toString();
         String stepDescription = edittextDescription.getText().toString();
 
 //            SAVE STEPS FOR SOP
-        StepsRoomData newStep = new StepsRoomData(sopTitle, stepTitle, stepNumber,stepDescription, imageUri.toString());
+        StepsRoomData newStep = new StepsRoomData(sopTitle, stepTitle, stepNumber, stepDescription, imageUri.toString());
         stepsRoomDatabase().listOfSteps().insertSteps(newStep);
     }
 
@@ -91,34 +115,92 @@ public class AddStep extends AppCompatActivity {
                 .build();
     }
 
-    public void setStepText(int stepNumber){
+    public void setStepText(int stepNumber) {
         String stepConcat = "Step" + stepNumber;
         textviewStepCount.setText(stepConcat);
     }
-
-    public void pickImageFromGallery(){
+//  THIS CREATS THE IMAGE AS A BUTTON TO OPEN GALLERY OR CAMERA
+    public void pickImageFromGallery() {
         imageviewGallery.setOnClickListener(v -> {
-            Intent getIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-            getIntent.setType("image/*");
-
-            Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            pickIntent.setType("image/*");
-
-            Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
-
-            startActivityForResult(chooserIntent, PICK_IMAGE);
+            ActivityCompat.requestPermissions(this,
+                    GALLERY_PERMISSIONS,
+                    PERMISSION_GALLERY);
+        });
+        imageviewCamera.setOnClickListener(v -> {
+            ActivityCompat.requestPermissions(this,
+                    CAMERA_PERMISSIONS,
+                    PERMISSION_CAMERA);
         });
     }
+//    THIS ASKS FOR PERMISSION FOR GALLER AND CAMERA AND CREATES INTENT
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+//            GALLERY PERMISSIONS
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // GALLERY PERMISSION GRANTED. THIS OPENS GALLERY CHOOSER
+                    Intent getIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    getIntent.setType("image/*");
+
+                    Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    pickIntent.setType("image/*");
+
+                    Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+                    startActivityForResult(chooserIntent, 2);
+                } else {
+                    // permission denied, boo!
+                    Toast.makeText(this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+//            CAMERA PERMISSIONS
+            case 2: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                    StrictMode.setVmPolicy(builder.build());
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    Date date = new Date();
+                    DateFormat df = new SimpleDateFormat("-mm-ss");
+
+                    String newPicFile = df.format(date) + ".jpg";
+                    String outPath = "/sdcard/" + newPicFile;
+                    File outFile = new File(outPath);
+
+                    mCameraFileName = outFile.toString();
+                    Uri outuri = Uri.fromFile(outFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+                    startActivityForResult(intent, 2);
+                } else {
+                    Toast.makeText(this, "Permission denied to ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    }
+//    THIS GETS RESULTS FROM GALLERY OR CAMERA INTENT AND SHOWS PREVIEW AND SAVES
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE) {
-            if(resultCode == RESULT_OK)
+//                RESULTS FROM CAMERA CAPTURE
+        if (requestCode == 2) {
+            if (data != null) {
                 imageUri = data.getData();
-            imageviewImagePreview.setVisibility(View.VISIBLE);
-            imageviewImagePreview.setImageURI(imageUri);
-            imageviewImagePreview.invalidate();
+                imageviewImagePreview.setImageURI(imageUri);
+                imageviewImagePreview.setVisibility(View.VISIBLE);
+            }
+            if (imageUri == null && mCameraFileName != null) {
+                imageUri = Uri.fromFile(new File(mCameraFileName));
+                imageviewImagePreview.setImageURI(imageUri);
+                imageviewImagePreview.setVisibility(View.VISIBLE);
+            }
         }
     }
 }
